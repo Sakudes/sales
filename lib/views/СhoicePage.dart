6cee5b5ec76846e51d9ad5d072cd8bf411d0c109
Saku <128635901+Sakudes@users.noreply.github.com>
2023-03-23 19:34:28 +0600

    assets
    assets with svg

--- lib/views/СhoicePage.dart
+++ lib/views/СhoicePage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:sales/widgets/Button.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ChoicePage extends StatefulWidget {
   const ChoicePage({Key? key}) : super(key: key);
@@ -16,12 +17,18 @@ class _ChoicePageState extends State<ChoicePage> {
     return Scaffold(
         appBar: AppBar(),
         body: Container(
-          padding:
-              const EdgeInsets.only(left: 20, right: 20, top: 40, bottom: 40),
-          child: const Button(
-            buttonName: 'Продолжить',
-            fullWidht: true,
-          ),
-        ));
+            padding:
+                const EdgeInsets.only(left: 20, right: 20, top: 40, bottom: 40),
+            child: Column(
+              children: [
+                SvgPicture.asset(
+                  'images/men_on_oclok.svg',
+                ),
+                Button(
+                  buttonName: 'Продолжить',
+                  fullWidht: true,
+                ),
+              ],
+            )));
   }
 }
